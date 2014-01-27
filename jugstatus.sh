f3b9003e2fc03306e530ed1ae3e6d82bf9e7933b
Matthew Jones <m.jones3@pgr.reading.ac.uk>
2014-01-27 14:36:31 +0000

    Monthly mean works, splitting netcdf files does not.

---
+++ jugstatus.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/usr/bin/jug_py27 status /home/users/mjones07/science/par_mon_mean.py &
---
+++ plotting/zonal_mean_plots.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2.7
+''' Zonal mean plots. 
+'''
+
+from netCDF4 import Dataset
+import numpy as np
+import matplotlib.pyplot as plt
+
